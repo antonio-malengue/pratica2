c96a56611795e1f6eff6e4e435cc80348c7d7095
Malengue <antoniocarlosmalengue@gmail.com>
2024-04-15 16:54:24 +0100

    com try exception

--- pratica2.py
+++ pratica2.py
@@ -1,20 +1,34 @@
 # Função principal
 def main():
-    # Solicita a data ao usuário
-    data = input("Digite uma data no formato 'dd/mm/aaaa': ")
-
-    # Separa a data em dia, mês e ano utilizando o método split() e o caractere '/'
-    dia, mes, ano = data.split("/")
-
-    # Converte as partes da data de string para inteiro
-    dia = int(dia)
-    mes = int(mes)
-    ano = int(ano)
+    condicao = True
+    while condicao:
+        try:
+            # Solicita a data ao usuário
+            data = input("Digite uma data no formato 'dd/mm/aaaa': ")
+            
+            # Separa a data em dia, mês e ano utilizando o método split() e o caractere '/'
+            dia, mes, ano = data.split("/")
+            
+            # Converte as partes da data de string para inteiro
+            dia = int(dia)
+            mes = int(mes)
+            ano = int(ano)
+            
+            # Exibe as partes da data separadamente
+            print("Dia:", dia)
+            print("Mês:", mes)
+            print("Ano:", ano)
+            
+            # Define a condição como False para sair do loop
+            condicao = False
+            
+        except ValueError as erro:
+            print("Erro:", erro)  # Imprime a mensagem de erro específica
+            print("Por favor, digite a data no formato correto.")
+            
 
     # Exibe as partes da data separadamente
-    print("Dia:", dia)
-    print("Mês:", mes)
-    print("Ano:", ano)
+    
 
 # Chamada da função principal
 if __name__ == "__main__":
